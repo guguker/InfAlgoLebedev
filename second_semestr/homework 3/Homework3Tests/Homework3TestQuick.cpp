7db4e2801a530309327d88c841ea899a1cb9afcb
guguker <valerijiy.124@gmail.com>
2025-04-08 20:23:54 +0300

    fucking final i guess

--- second_semestr/homework 3/Homework3Tests/Homework3TestQuick.cpp
+++ second_semestr/homework 3/Homework3Tests/Homework3TestQuick.cpp
@@ -23,35 +23,35 @@ namespace Homework3Tests
         TEST_METHOD(TestQuickSort_1_usual)  // массив с обычными числами
         {
             vector<int> arr = { 34, 23, 45, 67, 12, 89, 53, 17, 28, 92, 74, 56, 49, 77, 33, 8, 56, 15, 67, 99 };
-            quickSort(arr, 0, arr.size() - 1);
+            quickSort3Way(arr, 0, arr.size() - 1);
             Assert::IsTrue(isSorted(arr), L"Array is not sorted! :(");
         }
 
         TEST_METHOD(TestQuickSort_2_negative)  // массив с отрицательными числами
         {
             vector<int> arr = { -15, -34, -8, -45, -3, -23, -56, -12, -72, -89, -28, -92, -9, -64, -33, -77, -49, -41, -3, -18 };
-            quickSort(arr, 0, arr.size() - 1);
+            quickSort3Way(arr, 0, arr.size() - 1);
             Assert::IsTrue(isSorted(arr), L"Array is not sorted! :(");
         }
 
         TEST_METHOD(TestQuickSort_3_zero)  // массив из нулей
         {
             vector<int> arr = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            quickSort(arr, 0, arr.size() - 1);
+            quickSort3Way(arr, 0, arr.size() - 1);
             Assert::IsTrue(isSorted(arr), L"Array is not sorted! :(");
         }
 
         TEST_METHOD(TestQuickSort_4_moreThan100)  // массив с числами больше 100
         {
             vector<int> arr = { 120, 150, 110, 250, 130, 190, 200, 220, 170, 180, 300, 270, 140, 160, 230, 190, 1000, 300, 950, 400 };
-            quickSort(arr, 0, arr.size() - 1);
+            quickSort3Way(arr, 0, arr.size() - 1);
             Assert::IsTrue(isSorted(arr), L"Array is not sorted! :(");
         }
 
         TEST_METHOD(TestQuickSort_5_unoriginal)  // массив с одинаковыми числами
         {
             vector<int> arr = { 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 };
-            quickSort(arr, 0, arr.size() - 1);
+            quickSort3Way(arr, 0, arr.size() - 1);
             Assert::IsTrue(isSorted(arr), L"Array is not sorted! :(");
         }
     };
